170f98dacd17c444ed85958942f53978e1c56f55
SayakaIw <160105syk@gmail.com>
2022-08-28 10:22:23 +0000

    relation_start

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,15 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
+  # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定します。
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # そのユーザがフォローしている人の一覧を出したい
+  has_many :followings, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # そのユーザがフォローされている人の一覧を出したい
+  has_many :followe, through: :passive_relationships, source: :follower
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
@@ -16,4 +25,18 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
+  #フォローした時の処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  #フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  #フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
